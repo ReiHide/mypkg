8f3a6266abe1ccf51305967e13dfb1f4cb0127c2
Hide Rei <s21C1139wv@s.chibakoudai.jp>
2023-01-03 01:30:00 +0900

    change og content

--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Hideya Reizen
+# SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"
@@ -9,4 +11,5 @@ source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep 'Listen: 10'
+	grep 'Listen: 10'
+
